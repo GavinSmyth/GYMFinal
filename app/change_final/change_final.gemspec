d71c92fbc00de793e3407f8fc992551016db00dc
GavinSmyth <gavinsmyth1998@gmail.com>
2020-03-28 21:32:54 +0000

    9 Commit

---
+++ app/change_final/change_final.gemspec
@@ -0,0 +1,10 @@
+Gem::Specification.new do |s|
+    s.name = 'change_final'
+    s.version = '0.0.0'
+    s.date = '2020-02-02'
+    s.summary = "change  weight!"
+    s.description = "A simple gem"
+    s.authors = ["Gavin Smyth"]
+    s.email = 'gavinsmyth1998@gmail.ie'
+    s.files = ["lib/change_final.rb"]
+   end
---
+++ app/change_final/lib/change_final.rb
@@ -0,0 +1,46 @@
+require 'singleton'
+module ChangeFinal
+
+    class ConversionError < StandardError
+    end
+    class Different
+      include Singleton
+    # lookup table
+    CONVERSIONS = {
+      kg: {
+        pounds: 2.20462,
+        stone: 0.157473,
+      },
+      pounds: { 
+        kg: 0.453592,
+        stone: 0.0714286
+      },
+      stone: {
+        kg: 6.35029,
+        pounds: 14  
+      }
+    }
+    weight = Different.instance
+  
+    def convert(value, from, to:)
+      raise ConversionError, "Value is not numeric" unless value.is_a? Numeric
+      raise ConversionError, "Value #{value} is not positive" unless value >= 0
+      "#{value * conversion_ratio(from, to) }#{to.to_s}"
+    end
+
+    def conversions(value, unit)
+        CONVERSIONS[unit].keys.map do |other_unit|
+          convert(value, unit, to: other_unit)
+        end
+      end
+  
+    private
+    def conversion_ratio(from, to)
+      begin
+        CONVERSIONS.fetch(from).fetch(to)
+      rescue KeyError
+        raise ConversionError, "Cannot convert #{from.to_s} to #{to.to_s}"
+      end
+    end
+  end
+  end
